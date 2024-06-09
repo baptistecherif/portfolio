2b45bc8ad7ae5edd0c79e62704cb1630d736c19e
baptistecherif <baptiste.cherif@gmail.com>
2024-06-09 11:10:47 +0200

    Starting up App.jsx menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-const App = () => {
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
+const App = () => {
   return (
-    <div>
-      Baptiste Cherif Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+      </div>
+    </BrowserRouter>
   ) 
 }
 
