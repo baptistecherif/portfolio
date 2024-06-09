7a36390f477e765f347986fbde95b60950129c78
baptistecherif <baptiste.cherif@gmail.com>
2024-06-09 18:04:20 +0200

    Navbar.jsx almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,20 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
           <p className="text-white text-[18px] font-bold cursor-pointer">Baptiste <span className="sm:block hidden"> Cherif</span></p>
         </Link>
-        <p className="text-red-500">asdsa</p>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                ? "text-white"
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>            
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
