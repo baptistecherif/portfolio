8e642d8aeae9b050328835ec52f7396b2e29420b
baptistecherif <baptiste.cherif@gmail.com>
2024-06-11 21:35:44 +0200

    Project almost done need to check email and assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,16 +9,16 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
+//import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
+//import nodejs from "./tech/nodejs.png";
+//import reactjs from "./tech/reactjs.png";
+//import redux from "./tech/redux.png";
+//import tailwind from "./tech/tailwind.png";
+//import typescript from "./tech/typescript.png";
 
 //import meta from "./company/meta.png";
 //import shopify from "./company/shopify.png";
@@ -26,8 +26,8 @@ import jcizzo from "./company/jcizzo.png";
 //import tesla from "./company/tesla.png";
 
 import pong from "./pong.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+//import jobit from "./jobit.png";
+//import tripguide from "./tripguide.png";
 
 export {
   logo,
