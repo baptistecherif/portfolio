4ba60cc144ee7f375c699c655283e3cb8f38434e
baptistecherif <baptiste.cherif@gmail.com>
2024-06-13 21:02:01 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
